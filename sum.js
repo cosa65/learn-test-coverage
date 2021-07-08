a121320e3b6a0141c0347720eb62b8f9e7450879
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-07-08 12:19:45 -0300

    Add difference function

--- sum.js
+++ sum.js
@@ -1,5 +1,9 @@
 function sum(num1, num2) {
     return num1 + num2;
-  }
+}
+
+function difference(num1, num2) {
+    return num1 - num2;
+}
   
-module.exports = sum;
+module.exports = {sum, difference};
